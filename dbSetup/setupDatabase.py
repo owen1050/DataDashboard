c9d182309f8c198daa5cc935785eefab31e3d005
owen1050 <owen1050@gmail.com>
2024-10-15 12:04:08 -0400

    updated DB with start and end times

--- dbSetup/setupDatabase.py
+++ dbSetup/setupDatabase.py
@@ -5,9 +5,9 @@ cur = con.cursor()
 
 cur.execute("CREATE TABLE IF NOT EXISTS timestamps(lineID INTEGER,   stationID integer, state integer, timeOfEvent timestamp )")
 
-cur.execute("CREATE TABLE IF NOT EXISTS averageStationTimes(lineID integer, stationID integer, averageTimeSec real, updatedAt timestamp, updateCount integer)")
+cur.execute("CREATE TABLE IF NOT EXISTS averageStationTimes(lineID integer, stationID integer, averageTimeSec real, startTSID integer, endTSID integer)")
 
-cur.execute("CREATE TABLE IF NOT EXISTS lineEffeciencies(lineID integer,  lineEffeciency real,  updatedAt timestamp )")
+cur.execute("CREATE TABLE IF NOT EXISTS lineEffeciencies(lineID integer,  lineEffeciency real, startTSID integer, endTSID integer)")
 
 cur.execute("CREATE TABLE IF NOT EXISTS lineNames(lineID integer, lineName text)")
 
