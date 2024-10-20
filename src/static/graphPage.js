14f781f306fa2fc2ab969cea8e7dc6a5decccfd2
Will Busler <44672929+Ba11s@users.noreply.github.com>
2024-10-20 13:03:38 -0400

    graph works NEED MORE DATA @WILL BUSKLER

--- src/static/graphPage.js
+++ src/static/graphPage.js
@@ -12,29 +12,33 @@ function updateGraph(){
 
 function setupDemoData(){
 
-  let realData = getAllStationsAvgOverTime(lineID, 60,3600,3600)
+  let realData = getAllStationsAvgOverTime(lineID, 60,3600,3600*4)
 
   let oneStation = realData[1]
   console.log(oneStation)
+  timesForX = []
+  avgTimeOnY = []
 
   for(let i = 0; i < oneStation.length; i++){
     //x and y array fill out here with oneStation[i][0] and [1]
+    timesForX.push(oneStation[i][0])
+    avgTimeOnY.push(oneStation[i][1])
   }
   const xArray = [50,60,70,80,90,100,110,120,130,140,150];
   const yArray = [7,8,8,9,9,9,10,11,14,14,15];
 
   // Define Data
   const data = [{
-    x:xArray,
-    y:yArray,
-    mode:"markers"
+    x:timesForX,
+    y:avgTimeOnY,
+    mode:"scatter"
   }];
 
   // Define Layout
   const layout = {
-    xaxis: {range: [40, 160], title: "Square Meters"},
-    yaxis: {range: [5, 16], title: "Price in Millions"},  
-    title: "House Prices vs. Size"
+    xaxis: {title: "Time"},
+    yaxis: {title: "Avg Time (s)"},  
+    title: "Avg Cycle time over time"
   };
 
   // Display using Plotly
