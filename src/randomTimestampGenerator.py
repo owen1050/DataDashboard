63d4b1d3a7ec2c2489a92bb947bc871d016ed36c
Will Busler <44672929+Ba11s@users.noreply.github.com>
2024-10-20 17:36:34 -0400

    generated a lot of random data

--- src/randomTimestampGenerator.py
+++ src/randomTimestampGenerator.py
@@ -7,27 +7,29 @@ from datetime import timedelta
 db = databaseQuerys()
 
 timestepBack = 600
-timesToStepBack = 25
-datapointsToCreate = 20
+timesToStepBack = 30
+datapointsToCreate = 10
 spacing = 10
 avgTime = 10
-oneStepVariance = 6
+oneStepVariance = 10
 oneSessionVariance = 1
-lines = [6,7]
-stations = [1,2]
+lines = [11,12,13,14,15]
+stations = [1,2,3,4,5,6,7,8,10]
 
 startTime = datetime.now()
 
-for stepBack in range(timestepBack):
+for stepBack in range(timesToStepBack):
     
-    thisStepRandomDiff = (random() - 0.5) * oneStepVariance
+    thisStepRandomDiff = (random() - 0.5) * oneStepVariance + avgTime
     thisStepStartTime = startTime - timedelta(seconds = stepBack * timestepBack) + timedelta(seconds=thisStepRandomDiff)
     for datapoint in range(datapointsToCreate):
         for line in lines:
             for station in stations:
+                thisStationStartTime = thisStepStartTime + timedelta(seconds=spacing * datapoint)
                 thisStationRandomDiff =  (random() - 0.5) * oneSessionVariance
-                db.addTimestampEventAT(line, station, 0, thisStepStartTime)
-                db.addTimestampEventAT(line, station, 1, thisStepStartTime + timedelta(seconds=thisStationRandomDiff))
+
+                db.addTimestampEventAT(line, station, 0, thisStationStartTime)
+                db.addTimestampEventAT(line, station, 1, thisStationStartTime + timedelta(seconds=thisStationRandomDiff))
 
 
 
